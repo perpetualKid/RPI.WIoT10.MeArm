feebbe6adfea9249cb117c8175efa51cdad704a8
perpetualKid <perpetualKid@azuros.net>
2017-01-29 16:44:09 +0100

    Added UWP control app

---
+++ RPI.WIoT10.MeArm.Control/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPI.WIoT10.MeArm.Control
+{
+    public enum DeviceSettingNames
+    {
+        OneDriveClientId,
+        OneDriveClientSecret,
+        DeviceHost,
+        DevicePort
+    }
+}
---
+++ RPI.WIoT10.MeArm.Control/Views/OnboardCameraPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using RPI.WIoT10.MeArm.Control.Controller;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RPI.WIoT10.MeArm.Control.Views
+{
+    public sealed partial class OnboardCameraPage : Page
+    {
+        private ImageSourceController imageSource;
+
+        public OnboardCameraPage()
+        {
+            this.InitializeComponent();
+            //imageSource = ImageSourceController.Instance;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            imageSource.OnImageReceived += ImageSource_OnImageReceived;
+            imageSource.SupportedFormats.CollectionChanged += SupportedFormats_CollectionChanged;
+        }
+
+        private void SupportedFormats_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            //dropdownResolutions.Items.Clear();
+            //foreach(var item in imageSource.SupportedFormats)
+            //{
+            //    dropdownResolutions.Items.Add(item);
+            //}
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            imageSource.OnImageReceived -= ImageSource_OnImageReceived;
+            imageSource.SupportedFormats.CollectionChanged -= SupportedFormats_CollectionChanged;
+        }
+
+        private void ImageSource_OnImageReceived(object sender, EventArgs e)
+        {
+            this.imgMain.Source = imageSource.CurrentImage;
+        }
+
+        public ObservableCollection<BitmapImage> Items
+        {
+            get { return this.imageSource.CachedImages; }
+        }
+
+        private async void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            await imageSource.CaptureDeviceImage();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.imgMain.Source = this.imageSource.CachedImages[lvPictureCache.SelectedIndex];
+        }
+
+        private async void dropdownResolutions_DropDownOpened(object sender, object e)
+        {
+            await imageSource.GetSupportedModes();
+        }
+    }
+}
